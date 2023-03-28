159d8455aa6ad014ebccd9171e5a6c1daa77ca11
Evelin-lozano <evelinlozano163gmail.com>
2023-03-28 11:31:50 -0500

    first-commit

---
+++ formulario.js
@@ -0,0 +1,17 @@
+const FORMULARIO = document.querySelector("#formulario")
+
+FORMULARIO.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    const user = event.target.user.value.trim().toLowerCase()
+    const Apellido = event.target.Apellido.value.trim().toLowerCase()
+    const email = event.target.email.value.trim().toLowerCase()
+    const pwd = event.target.pwd.value.trim()
+
+    console.log(user)
+    console.log(Apellido)
+    console.log(email)
+    console.log(pwd)
+
+
+})
